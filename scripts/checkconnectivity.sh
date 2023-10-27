98f816047539d365d7336bb8380bd9f7baecdc49
Mitesh Panchal <145320555+miteshp98@users.noreply.github.com>
2023-10-27 19:02:36 +0530

    Added New Script

---
+++ scripts/checkconnectivity.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Version-1.0
+#Created by Mitesh Panchal.
+#Script is about Checking server connectivity & If Server Goes Down It Will Nofify You.
+
+#Define server name.
+
+servername="192.168.68.122"
+
+#Checking Connectivity If then.
+
+if ping -c 3 $servername > /dev/null ; then
+	echo "Server is Running Good and Is UP"
+else
+	notify-send -u critical "Server Down" "$servername is Not Reachable"
+fi
