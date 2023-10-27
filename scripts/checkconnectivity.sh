bd2ba6ab61aba2234610bca69202ff1ce64f8435
Mitesh Panchal <145320555+miteshp98@users.noreply.github.com>
2023-10-27 19:26:38 +0530

    Update checkconnectivity.sh

--- scripts/checkconnectivity.sh
+++ scripts/checkconnectivity.sh
@@ -5,13 +5,14 @@
 #Script is about Checking server connectivity & If Server Goes Down It Will Nofify You.
 
 #Define server name.
-
 servername="www.name.com"
+#Define Directory To save Status log.
+logdir="/path/to/server.log"
 
 #Checking Connectivity If then.
 
 if ping -c 3 $servername > /dev/null ; then
-	echo "Server is Running Good and Is UP"
+	echo "Server is Running Good and is UP: $(date '+%d-%m-%Y_%H:%M')" >> "$logdir"  2>&1
 else
 	notify-send -u critical "Server Down" "$servername is Not Reachable"
 fi
